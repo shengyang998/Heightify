4e15e7c6083eed4db3fa90309d10ba99d23c1264
Soleil <shengyang998@gmail.com>
2025-02-15 14:49:09 +0800

    Initial Commit

---
+++ Heightify/HeightifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeightifyApp.swift
+//  Heightify
+//
+//  Created by Soleil Yu on 2025/2/15.
+//
+
+import SwiftUI
+
+@main
+struct HeightifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeightifyTests/HeightifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  HeightifyTests.swift
+//  HeightifyTests
+//
+//  Created by Soleil Yu on 2025/2/15.
+//
+
+import Testing
+
+struct HeightifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
